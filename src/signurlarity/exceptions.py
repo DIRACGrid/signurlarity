407153cca0fba24ca473cf2663a6ef6dae9fa83d
Christophe Haen <christophe.haen@cern.ch>
2026-02-24 10:11:24 +0100

    chore: introduce SignurlarityError exception

--- src/signurlarity/exceptions.py
+++ src/signurlarity/exceptions.py
@@ -1,47 +1,46 @@
 from __future__ import annotations
 
 
-class PresignError(Exception):
+class SignurlarityError(Exception): ...
+
+
+class PresignError(SignurlarityError):
     """Base exception for presigning errors."""
 
     pass
 
 
-class InvalidURLError(PresignError):
+class InvalidURLError(SignurlarityError):
     """Raised when URL format is invalid."""
 
     pass
 
 
-class ExpirationError(PresignError):
+class ExpirationError(SignurlarityError):
     """Raised when expiration parameters are invalid."""
 
     pass
 
 
-class SignatureError(PresignError):
+class SignatureError(SignurlarityError):
     """Raised when signature generation fails."""
 
     pass
 
 
-class NoSuchBucketError(PresignError):
+class NoSuchBucketError(SignurlarityError):
     """Raised when bucket does not exist or is not accessible."""
 
     pass
 
 
-class BucketAlreadyExistsError(PresignError):
+class BucketAlreadyExistsError(SignurlarityError):
     """Raised when trying to create a bucket that already exists."""
 
     pass
 
 
-class BucketAlreadyOwnedByYouError(PresignError):
+class BucketAlreadyOwnedByYouError(SignurlarityError):
     """Raised when trying to create a bucket you already own."""
 
-    pass
-
-
-class ClientError(Exception):
     pass
